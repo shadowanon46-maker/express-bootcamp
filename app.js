90ada7b7ba0f052fec0f52596bbc8b89f953676a
Fitrah Andre <shadowanon46@gmail.com>
2025-11-27 17:08:31 +0700

    feat: add response json app.js

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 const port = 3000;
 
+app.use(express.json());
 
 app.get('/', (req, res) => {
-  res.send('Hello World!');
+  res.json({ message: 'Hello, World!' });
 });
 
 app.listen(port, () => {
